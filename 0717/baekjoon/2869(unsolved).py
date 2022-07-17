f5f4d7668abba6422ffb58a81142ce7da0bf7123
qlghwp123 <qlghwp123@naver.com>
2022-07-18 01:32:14 +0900

    Update TIL and unsolved file

---
+++ 0717/baekjoon/2869(unsolved).py
@@ -0,0 +1,24 @@
+A, B, V = map(int, input().split())
+
+# 아래는 처음 작성한 코드.
+# 하지만 시간이 너무 오래 걸리고 애초 문제에서 제시한 시간 제한을 아득히 초과한다.
+
+# result = 0
+# i = 0
+# while result < V:
+#     i += 1
+#     result += A
+
+#     if result >= V:
+#         break
+#     else:
+#         result -= B
+
+# print(i)
+
+if V - B == 0:
+    result = 1
+else:
+    result = V - B // A - B
+
+print(result)
